0d947588fb6a4eff5a0997e1ed02239899c346e1
S-DEV <shmoulik.an@gmail.com>
2024-12-17 16:31:05 +0100

    Update allowed hosts configuration

--- vparrot/settings.py
+++ vparrot/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
-ALLOWED_HOSTS = ['98.83.115.236', '172-31-38-53', "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "admin_interface",
